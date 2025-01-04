06820d8668e42ccf8c6f5110d0334fc82369140d
Stephen Ostapenko <stephen@ostapenko.com>
2025-01-05 01:15:43 +0300

    hw8 done

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,1 +1,97 @@
-// TODO
+__kernel void set_zeros(__global unsigned int *a, unsigned int n) {
+    int idx = get_global_id(0);
+    if (idx < n) {
+        a[idx] = 0;
+    }
+}
+
+#define TRANSPOSE_TILE_SIZE 16
+
+__kernel void matrix_transpose_local_memory_kernel(
+    __global const unsigned int *a,
+    __global unsigned int *a_t,
+    unsigned int m, unsigned int k
+) {
+    unsigned int i = get_global_id(0);
+    unsigned int j = get_global_id(1);
+
+    if (j >= m || i >= k) {
+        barrier(CLK_LOCAL_MEM_FENCE);
+        return;
+    }
+
+    unsigned int local_i = get_local_id(0);
+    unsigned int local_j = get_local_id(1);
+
+    __local float tile[TRANSPOSE_TILE_SIZE][TRANSPOSE_TILE_SIZE + 1];
+
+    tile[local_j][local_i] = a[j * k + i];
+
+    unsigned int res_i = get_group_id(0) * TRANSPOSE_TILE_SIZE + local_j;
+    unsigned int res_j = get_group_id(1) * TRANSPOSE_TILE_SIZE + local_i;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (res_i < k && res_j < m) {
+        a_t[res_i * m + res_j] = tile[local_i][local_j];
+    }
+}
+
+__kernel void prefix_sum_kernel(
+    __global const unsigned int *a, __global unsigned int *b,
+    unsigned int n, unsigned int start, int step, unsigned int offset
+) {
+    int pos = start + get_global_id(0) * step;
+    if (pos < 0 || pos >= n) {
+        return;
+    }
+    b[pos] = a[pos];
+    if (pos >= offset) {
+        b[pos] += a[pos - offset];
+    }
+}
+
+unsigned int get_bits(unsigned int x, unsigned int bits_num, unsigned int shift) {
+    return (x >> shift) & ((1 << bits_num) - 1);
+}
+
+__kernel void calc_cnt_kernel(
+    __global const unsigned int *a, __global unsigned int *cnt,
+    unsigned int n, unsigned int bits_num, unsigned int shift
+) {
+    unsigned int gid = get_global_id(0);
+    unsigned int wid = get_group_id(0);
+    const unsigned int total_counters = 1 << bits_num;
+
+    if (gid < n) {
+        atomic_inc(&cnt[wid * total_counters + get_bits(a[gid], bits_num, shift)]);
+    }
+}
+
+__kernel void radix_sort_kernel(
+    __global const unsigned int *a, __global unsigned int *b,
+    __global const unsigned int *cnt_t,
+    unsigned int n, unsigned int bits_num, unsigned int shift, unsigned int workgroups_num
+) {
+    unsigned int gid = get_global_id(0);
+    if (gid >= n) {
+        return;
+    }
+
+    unsigned int lid = get_local_id(0);
+    unsigned int wid = get_group_id(0);
+    unsigned int digit = get_bits(a[gid], bits_num, shift);
+
+    unsigned int eq_cnt = 0;
+    for (int i = 1; i <= lid; i++) {
+        eq_cnt += (get_bits(a[gid - i], bits_num, shift) == digit);
+    }
+
+    unsigned int lt_cnt = 0;
+    unsigned int cnt_pos = digit * workgroups_num + wid;
+    if (cnt_pos > 0) {
+        lt_cnt = cnt_t[cnt_pos - 1];
+    }
+
+    b[lt_cnt + eq_cnt] = a[gid];
+}
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -42,6 +42,29 @@ std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
     return cpu_sorted;
 }
 
+void calc_prefix_sum(
+    ocl::Kernel& prefix_sum, gpu::gpu_mem_32u& a_gpu,
+    unsigned int n, unsigned int workgroup_size
+) {
+    unsigned int offset;
+    for (offset = 1; offset < n; offset <<= 1) {
+        prefix_sum.exec(
+            gpu::WorkSize(workgroup_size, n / (offset * 2)),
+            a_gpu, a_gpu,
+            n, offset * 2 - 1, offset * 2, offset
+        );
+    }
+
+    offset >>= 1;
+    for (offset >>= 1; offset > 0; offset >>= 1) {
+        prefix_sum.exec(
+            gpu::WorkSize(workgroup_size, n / (offset * 2)),
+            a_gpu, a_gpu,
+            n, offset * 3 - 1, offset * 2, offset
+        );
+    }
+}
+
 int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
@@ -58,21 +81,77 @@ int main(int argc, char **argv) {
 
     const std::vector<unsigned int> cpu_reference = computeCPU(as);
 
-    // remove me
-    return 0;
+    ocl::Kernel set_zeros(radix_kernel, radix_kernel_length, "set_zeros");
+    set_zeros.compile();
+    ocl::Kernel matrix_transpose_kernel(radix_kernel, radix_kernel_length, "matrix_transpose_local_memory_kernel");
+    matrix_transpose_kernel.compile();
+    ocl::Kernel prefix_sum_kernel(radix_kernel, radix_kernel_length, "prefix_sum_kernel");
+    prefix_sum_kernel.compile();
+    ocl::Kernel calc_cnt_kernel(radix_kernel, radix_kernel_length, "calc_cnt_kernel");
+    calc_cnt_kernel.compile();
+    ocl::Kernel radix_sort_kernel(radix_kernel, radix_kernel_length, "radix_sort_kernel");
+    radix_sort_kernel.compile();
+
+    const unsigned int workgroup_size = 128;
+    const unsigned int transpose_tile_size = 16;
+    const unsigned int bits_num = 4;
+    const unsigned int counter_size = 1 << bits_num;
+    const unsigned int workgroups_num = (n + workgroup_size - 1) / workgroup_size;
+    const unsigned int total_counters_size = workgroups_num * counter_size;
+
+    gpu::gpu_mem_32u a_gpu, b_gpu, cnt_gpu, cnt_t_gpu;
+    a_gpu.resizeN(n);
+    b_gpu.resizeN(n);
+    cnt_gpu.resizeN(total_counters_size);
+    cnt_t_gpu.resizeN(total_counters_size);
 
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            // TODO
+            a_gpu.writeN(as.data(), as.size());
+
+            t.restart();
+
+            for (int shift = 0; shift < 32; shift += bits_num) {
+                set_zeros.exec(
+                    gpu::WorkSize(workgroup_size, total_counters_size),
+                    cnt_gpu, total_counters_size
+                );
+
+                calc_cnt_kernel.exec(
+                    gpu::WorkSize(workgroup_size, n),
+                    a_gpu, cnt_gpu,
+                    n, bits_num, shift
+                );
+
+                matrix_transpose_kernel.exec(
+                    gpu::WorkSize(transpose_tile_size, transpose_tile_size, counter_size, workgroups_num),
+                    cnt_gpu, cnt_t_gpu,
+                    workgroups_num, counter_size
+                );
+
+                calc_prefix_sum(prefix_sum_kernel, cnt_t_gpu, total_counters_size, workgroup_size);
+
+                radix_sort_kernel.exec(
+                    gpu::WorkSize(workgroup_size, n),
+                    a_gpu, b_gpu, cnt_t_gpu,
+                    n, bits_num, shift, workgroups_num
+                );
+
+                std::swap(a_gpu, b_gpu);
+            }
+
+            t.nextLap();
         }
         t.stop();
 
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
+
+    a_gpu.readN(as.data(), n);
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
